79ea9232b21d238a13f9e7961ac981ab046a5161
samimud <samimud@umich.edu>
2023-10-30 19:36:43 -0400

    gameSession + questionBuilder finished and tested

--- BNTRserver/questionBuilder.py
+++ BNTRserver/questionBuilder.py
@@ -34,8 +34,6 @@ class gameSession:
         team1_response = requests.get(url=team1_url,json={"api_key": self.BANTER_API_KEY}).json()
         team2_response = requests.get(url=team2_url,json={"api_key": self.BANTER_API_KEY}).json()
         
-        LOGGER.info("Sanity check")
-        
         self.team1 = team1_response["name"]
         
         
@@ -124,8 +122,6 @@ class gameSession:
              "question": "How many goals will there be in the second half?",
              "Game_id": self.gameID}
         ]
-        
-        self.run_game_session()
     def run_game_session(self):
         """Main thread for running the game"""
         self.build_questions("pregame")
@@ -138,6 +134,8 @@ class gameSession:
         while self.game_status == "IN PLAY":
             # Check for game time
             self.track_game_time()
+            LOGGER.info(f"Loop {self.debug_index}")
+            self.debug_index += 1
             if 20 <= self.game_time <= 30 and self.game_stage == "1H" and ingame_flag == False:
                 ingame_flag = True
                 self.build_questions("ingame")
@@ -148,7 +146,7 @@ class gameSession:
                 LOGGER.info("Creating halftime questions")
                 self.update_scores("halftime")
                 halftime_flag = True
-            time.sleep(60)
+            time.sleep(5)
         self.update_scores("final")
         self.resolve_questions()
         LOGGER.info("Resolving question answers")
@@ -229,7 +227,7 @@ class gameSession:
         
         ENDPOINT = f'https://api.the-odds-api.com/v4/sports/{SPORT}/events/'
         if self.DEBUG == True:
-            with open("testing/find_event_id.json", 'r') as file:
+            with open("BNTRserver/testing/find_event_id.json", 'r') as file:
                 odds_response = json.load(file)
         else:
             odds_response = requests.get(
@@ -264,7 +262,7 @@ class gameSession:
         ENDPOINT = f'https://api.the-odds-api.com/v4/sports/{SPORT}/events/{EVENT_ID}/odds'
         
         if self.DEBUG == True:
-            with open("testing/sportsbookOdds.json", 'r') as file:
+            with open("BNTRserver/testing/sportsbookOdds.json", 'r') as file:
                 odds_response = json.load(file)
                 return odds_response
         else:
@@ -297,10 +295,10 @@ class gameSession:
         
         if self.DEBUG == True:
             if self.DEBUG_HT == True:
-                filename = "testing/get_scores_halftime.json"
+                filename = "BNTRserver/testing/get_scores_halftime.json"
                 self.DEBUG_HT = False
             else:
-                filename = "testing/get_scores_final.json"
+                filename = "BNTRserver/testing/get_scores_final.json"
             with open(filename, 'r') as file:
                 odds_response = json.load(file)
                 return odds_response
@@ -353,7 +351,7 @@ class gameSession:
             "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
         }
         if self.DEBUG == True:
-            with open(f"testing/track_game_time_{self.debug_index}.json", 'r') as file:
+            with open(f"BNTRserver/testing/track_game_time_{self.debug_index}.json", 'r') as file:
                 data = json.load(file)
         else:
             response = requests.get(url, headers=headers, params=query)
@@ -361,8 +359,9 @@ class gameSession:
         
         self.game_time = data["response"][0]["fixture"]["status"]["elapsed"]
         self.game_stage = data["response"][0]["fixture"]["status"]["short"]
-        self.game_status = "Finished" if data["response"][0]["status"]["long"] == "Match Finished" else "IN PLAY"
-        
+        self.game_status = "Finished" if data["response"][0]["fixture"]["status"]["long"] == "Match Finished" else "IN PLAY"
+        if self.game_status == "Finished":
+            LOGGER.info("Game finished")
         return
     
     def locate_fixture_id(self):
@@ -377,7 +376,7 @@ class gameSession:
         query = {"league": str(self.prem_league_id)}
         
         if self.DEBUG == True:
-            with open(f"testing/locate_fixture_id.json", 'r') as file:
+            with open(f"BNTRserver/testing/locate_fixture_id.json", 'r') as file:
                 data = json.load(file)
         else:
             response = requests.get(url, headers=headers, params=query)
@@ -414,8 +413,8 @@ class gameSession:
                 "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
         }
         if self.DEBUG == True:
-            with open(f"testing/postgame_statistics.json", 'r') as file:
-                statistics = json.load(file)
+            with open(f"BNTRserver/testing/postgame_statistics.json", 'r') as file:
+                statistics = json.load(file)["response"]
         else:
             response = requests.get(stat_url, headers=headers, params=querystring)
             
@@ -446,23 +445,19 @@ class gameSession:
         h1_total = self.team1_goals["halftime"] + self.team2_goals["halftime"]
         
         h2_total = (self.team1_goals["final"] - self.team2_goals["halftime"]) + (self.team1_goals["final"] - self.team2_goals["halftime"])
-        
         for question in question_list:
             label = question["label"]
             answer = None
                 
                 
             if label == "h2h":
-                mapping = {question[key][0]: key for key in ["opt1", "opt2", "opt3"]}
-                answer = mapping.get(winning_team, None)
+                answer = f"opt{question['options'].index(winning_team) + 1}"
                 
             elif label == "h2h_h1":
-                mapping = {question[key][0]: key for key in ["opt1", "opt2", "opt3"]}
-                answer = mapping.get(h1_winner, None)
+                answer = f"opt{question['options'].index(h1_winner) + 1}"
             
             elif label == "h2h_h2":
-                mapping = {question[key][0]: key for key in ["opt1", "opt2", "opt3"]}
-                answer = mapping.get(h2_winner, None)
+                answer = f"opt{question['options'].index(h2_winner) + 1}"
                 
             elif label == "spreads":
                 if winning_team in self.team1_goals.values():
@@ -488,15 +483,15 @@ class gameSession:
                 answer = self.totals_helper(question, h2_total)
             
             elif label == "btts":
-                answer = "Yes" if (self.team1_goals["final"] > 0 and self.team2_goals["final"] > 0) else "No"
+                answer = "opt1" if (self.team1_goals["final"] > 0 and self.team2_goals["final"] > 0) else "opt2"
 
             elif label == "yellow_cards":
                 tot_yc = 0
                 for team in statistics:
                     for event in team["statistics"]:
                         if event["type"] == "Yellow Cards":
-                            tot_yc += event["value"]
-                            break
+                            val = 0 if event["value"] is None else event["value"]
+                            tot_yc += val
                 answer = "opt1" if tot_yc > 5 else "opt2"
                 
             elif label == "red_card":
@@ -504,10 +499,11 @@ class gameSession:
                 for team in statistics:
                     for event in team["statistics"]:
                         if event["type"] == "Red Cards":
-                            tot_rc += event["value"]
-                            break
+                            val = 0 if event["value"] is None else event["value"]
+                            tot_rc += val
                 answer = "opt1" if tot_rc > 0 else "opt2" 
-            
+            else:
+                answer = None
             # Pass answer into database using Banter API
             data = {
                 "api_key": self.BANTER_API_KEY,
@@ -521,37 +517,35 @@ class gameSession:
                 print("POST Request | Question Answer -> Database | Successful")
             else:
                 print('Request failed with status code:', response.status_code)
-        
-            return
+        return
     
-    def spread_helper(self, team_name, goal_diff, data):
+    def spread_helper(self, team_name, goal_diff, question):
         """Spread resolving helper"""
-        for key in ["opt1", "opt2"]:
-            option_team = data[key][0].split()[0]
-            option_spread = float(data[key][0].split()[1])
+        for idx, option in enumerate(question['options']):
+            if idx > 1:
+                break
+            option_team, option_spread = option.split()
+            option_spread = float(option_spread)
 
-            if team_name == option_team:
-                if team_name == data["opt1"][0].split()[0]:  # If the winning team is the team from opt1
-                    if goal_diff >= option_spread:  # Check if they won by at least the spread
-                        return key
-                else:  # If the winning team is the team from opt2
-                    if -goal_diff <= option_spread:  # Check if they won by at least the negative spread
-                        return key
+            # If the team matches and the goal difference exceeds the spread
+            if team_name == option_team and ((goal_diff >= 0 and goal_diff >= option_spread) or (goal_diff < 0 and goal_diff <= option_spread)):
+                return 'opt' + str(idx + 1)
         return None
 
     
     
-    def totals_helper(self, data, goals_scored):
+    def totals_helper(self, question, total_goals):
         """Totals helper."""
-        threshold = float(data["opt1"][0].split()[1])  
-
-        if goals_scored > threshold:
-            return "opt1"
-        else:
-            return "opt2"
+        threshold = float(question['options'][0].split()[1])
         
+        if total_goals > threshold:
+            return "opt1"  
+        else:
+            return "opt2" 
+            
         
     def question_testing(self, question):
         """Checking the questions"""
         with open("test_file.json", 'a') as file:
             json.dump(question, file, indent = 4, ensure_ascii=False)
+
